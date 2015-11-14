96a643ddb1d94fbfdd9567bbc5980cfb987ae3e3
Katherine Chao <kchao1994@gmail.com>
2015-11-13 22:31:47 -0800

    removed images again

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@ var imageSrcs = [
 	"art/spider.png",
 	"art/amsterdam.png",
 	"art/birdmask.png",
-	"art/china.png",
+	//"art/china.png",
 	"art/mask.png",
     "art/underwater.png",
     "art/girl-watercolor4.png",
